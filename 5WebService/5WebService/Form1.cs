dedc79024496ce4ca91fea6d8beead873bdf992e
Zsófia Tóth <zsofia.toth6@stud.uni-corvinus.hu>
2022-10-17 14:13:16 +0200

    calling webservice

--- 5WebService/5WebService/Form1.cs
+++ 5WebService/5WebService/Form1.cs
@@ -12,9 +12,33 @@ namespace _5WebService
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            ExchangeRates();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExchangeRates()
+        {
+            var mnbService = new ServiceReference1.MNBArfolyamServiceSoapClient();
+
+            var request = new ServiceReference1.GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            //richTextBox1.Text = result;
         }
     }
 }
