18609701bee8a31cc2d2b2b5271b21ecc09745bb
Zsófia Tóth <zsofia.toth6@stud.uni-corvinus.hu>
2022-11-25 12:18:45 +0100

    3A approach

---
+++ 8UnitTestExample/UnitTestExample/AccountControllerTestFixture.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample
+{
+    class AccountControllerTestFixture
+    {        
+
+        [Test]
+        public void TestValidateEmail(String email, bool expectedRes)
+        {
+            // Arrange
+            var accountController = new AccountController();
+            //Act
+            var actualResult = accountController.ValidateEmail(email);            
+            //Assert
+            Assert.AreEqual(expectedRes, actualResult);
+
+        }
+    }
+
+
+
+}
