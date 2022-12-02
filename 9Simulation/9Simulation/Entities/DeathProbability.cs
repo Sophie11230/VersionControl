35da85a012aacb092712e71f4782b1e4aa8dabe5
Zsófia Tóth <zsofia.toth6@stud.uni-corvinus.hu>
2022-12-02 12:04:06 +0100

    creating classes

---
+++ 9Simulation/9Simulation/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9Simulation.Entities
+{
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+        public Gender gender { get; set; }
+        public double pDeath { get; set; }
+    }
+}
--- 9Simulation/9Simulation/Form1.cs
+++ 9Simulation/9Simulation/Form1.cs
@@ -16,5 +16,10 @@ namespace _9Simulation
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
